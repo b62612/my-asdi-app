a2008a35138d1ee5a7240a1de42c22bccba8a8fe
Ejiroghene Efevberha <ejay.efe@fullsail.edu>
2014-02-13 23:17:36 -0500

    staging for week 2 submission

--- javascript/libraries/main.js
+++ javascript/libraries/main.js
@@ -4,8 +4,54 @@ Deliverable 1
 Add Player App
 ASDI 1402
 */
+$(document).ready(function(){ 
 
 
-$( "#button" ).(click, function() {
-  alert( "my jQuery Handler" );
+
+			
+			function getCheckboxActive(){
+					if($("#urgent").checked){
+						urgencyValue = $("#urgent").val();
+					}else{
+						urgencyValue = "No";
+					}		
+			}
+
+		$( ".submit" ).on('click', function(key) {
+ 		localStorage.key(0);
+
+  					
+		var id = Math.floor(Math.random()*10000000001);	
+					
+									
+			
+			getCheckboxActive();
+			urgencyValue;					
+					var item 			= {};
+						item.Age_Grades	= ["Age Category:", $("#agegrade").val()];
+						item.First_Name	= ["First Name:", $("#firstname").val()];
+						item.Last_Name	= ["Last Name:", $("#lastname").val()]; 
+						item.Email		= ["Agent Email:", $("#email").val()];  
+						item.Phone		= ["Agent Phone:", $("#phone").val()];
+						item.Position 	= ["Field Position:", $("#position").val()];
+						item.Free_Agent = ["Free Agent?:", urgencyValue];
+						item.Rating 	= ["Player Rating:", $("#rating").val()];
+						item.Date 		= ["Observation Date:", $("#date").val()];
+										
+			localStorage.setItem(id, JSON.stringify(item));	
+			alert("Player Saved!");
+
+
+
+  		});
+
+
+		$( "#results-af" ).on('click', function(key) {
+			
+
+
+		});
+
+
 });
+
